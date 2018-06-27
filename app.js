a7df3c2550c9900f7bd7a1e9ea765dfb37969fe3
lian1925 <lian1925@qq.com>
2018-06-27 14:38:10 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+// 跨域配置
+app.all('*', function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");  
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+  res.header("X-Powered-By",' 3.2.1')  
+  res.header("Content-Type", "application/json;charset=utf-8");  
+  next();  
+});  
+
+var indexRouter = require('./router/index');
+var usersRouter = require('./router/user');
+
+
+// 匹配根路由
+app.use('/', indexRouter);
+// 匹配user路由
+app.use('/user', usersRouter);
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ router/user.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+
+var jwt = require('jsonwebtoken');
+
+
+/* GET user page. */
+router.get('/', function(req, res, next) {
+  res.send({
+    message:"user page"
+  });
+});
+
+router.post('/login', function(req,res,next){
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var token =   jwt.sign({
+    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    username: username
+  }, 'secret');
+
+  if(username == "admin"&& password == "123"){
+    return res.send({
+      err_code: 0,
+      token: token
+    })
+  }
+  return res.send({
+    err_code: 1,
+    token:null
+  })
+
+})
+
+router.post('/info', function(req,res,next){
+
+  var token = req.body.token;
+
+  // verify a token symmetric
+  jwt.verify(token, 'secret', function(err, decoded) {
+    console.log(decoded.foo) // bar
+
+    if(decoded.username == "admin"){
+      return res.send({
+        err_code: 0,
+        username: 'admin',
+        age: 20
+      })
+    }else{
+      return res.send({
+        err_code: 1,
+        username: null
+      })
+    }
+  })
+})
+
+
+module.exports = router;
